9a10005c5e0300158d47ab67e5ccf7a024375e70
lgn <luisgaylornobre@gmail.com>
2022-12-12 19:26:40 -0500

    Add 2022 day 4

--- 22/py/04.py
+++ 22/py/04.py
@@ -1,3 +1,15 @@
 with open('../4.txt', 'r') as f:
-        for line in f.readlines():
+  part1, part2 = 0, 0
+  for line in f.readlines():
+    pair = [int(x) for x in line.strip().replace('-', ',').split(',')]
 
+    if ((pair[0] >= pair[2] and pair[1] <= pair[3]) or (pair[2] >= pair[0] and pair[3] <= pair[1])):
+      part1 += 1
+
+    overlaps = [x for x in range(pair[0], pair[1] + 1) if x in range(pair[2], pair[3] + 1)]
+    overlaps2 = x for x in range(pair[2], pair[3] + 1) if x in range(pair[0], pair[1] + 1)]
+    if(overlaps or overlaps2):
+      part2 += 1
+
+print(f'Part 1: {part1} pairs contained in each other')
+print(f'Part 2: {part2} pairs overlap')
